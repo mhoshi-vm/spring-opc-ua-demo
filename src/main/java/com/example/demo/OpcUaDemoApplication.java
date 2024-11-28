295f6f8f317e2ad052a297d10d48011a3aef9e0c
mhoshi-vm <machi.hoshino@broadcom.com>
2024-11-28 18:00:27 +0900

    Optimize

--- src/main/java/com/example/demo/OpcUaDemoApplication.java
+++ src/main/java/com/example/demo/OpcUaDemoApplication.java
@@ -3,11 +3,9 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
-@EnableScheduling
 @EnableConfigurationProperties(OpcUaClientProperties.class)
 public class OpcUaDemoApplication {
 
